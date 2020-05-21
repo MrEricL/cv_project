ebad6909fe9d5dbce59ca7b45f9645efc3cb35d1
benjaminlgur <benjaminlgur@yahoo.com>
2020-05-21 17:06:10 -0400

    new playsounds for expanded features

--- position_finder.py
+++ position_finder.py
@@ -93,7 +93,7 @@ while True:
         #print(center)
 
         # only proceed if the radius meets a minimum size
-        if radius > 5:
+        if radius > 10:
             # draw the circle and centroid on the frame,
             # then update the list of tracked points
             #cv2.circle(frame, (int(x), int(y)), int(radius), (0, 255, 255), 2)
